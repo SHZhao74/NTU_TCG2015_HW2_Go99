f38904dcbcdc93167a4032f0cfd94e56aa12ef6a
SHZhao74 <j5201314peter@gmail.com>
2015-12-10 20:21:04 +0800

    rewrit the template code

--- MCS.cc
+++ MCS.cc
@@ -1,5 +1,5 @@
 #include <math.h>
 #include "MCS.hpp"
-float UCB (int num_win, int num_play_M, int num_play_v, int c){
-	return (float)num_win/num_play_M + c*sqrt(log(num_play_v)/num_play_M);
+float compute_UCB (){
+	return (float)num_win/num_play_total + c_wight*sqrt(log(num_play_parent)/num_play_total);
 }
--- MCS.hpp
+++ MCS.hpp
@@ -1,14 +1,17 @@
 #ifndef MCS_H
 #define MCS_H
+int c_wight = 1;
 
-class node{
+class MCSnode{
 public:
-
+  MCSnode(){}
+  
 private:
   float UCB;
   int num_win;
   int num_play_total;
-
+  int num_play_parent;
+  MCSnode* child;
 };
 
 #endif
